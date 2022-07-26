a3ac32f4e0fd6d35ef84c959b99c9b4180d67997
Joshua Gomez <joshuago78@gmail.com>
2022-07-26 15:52:42 -0700

    converted tabs to spaces

--- indexer.py
+++ indexer.py
@@ -8,71 +8,71 @@ INDEX_FILE = 'KEYWORD_INDEX.md'
 
 
 def extract_keywords(path, pattern):
-	with open(path, 'r') as file:
-		text = file.read()
-	result = pattern.search(text)
-	if result is None or len(result.groups()) < 1:
-		raise Exception('Keywords section not found')
-	terms = [t.strip(' \'"') for t in result.group(1).split(',')]
-	return [f'{t[0].upper()}{t[1:]}' for t in terms]
+    with open(path, 'r') as file:
+        text = file.read()
+    result = pattern.search(text)
+    if result is None or len(result.groups()) < 1:
+        raise Exception('Keywords section not found')
+    terms = [t.strip(' \'"') for t in result.group(1).split(',')]
+    return [f'{t[0].upper()}{t[1:]}' for t in terms]
 
 
 def get_snippet_filepaths(dirs):
-	paths = []
-	for directory in dirs:
-		for file in os.listdir(directory):
-			if not file.endswith('.md'):
-				continue
-			path = os.path.join(directory, file)
-			paths.append(path)
-	return paths
+    paths = []
+    for directory in dirs:
+        for file in os.listdir(directory):
+            if not file.endswith('.md'):
+                continue
+            path = os.path.join(directory, file)
+            paths.append(path)
+    return paths
 
 
 def build_index(paths, pattern):
-	index = {}
-	errors = []
-	for path in paths:
-		try:
-			keywords = extract_keywords(path, pattern)
-			for term in keywords:
-				if term not in index.keys():
-					index[term] = []
-				index[term].append(path)
-		except Exception as e:
-			errors.append((path, e))
-	return index, errors
+    index = {}
+    errors = []
+    for path in paths:
+        try:
+            keywords = extract_keywords(path, pattern)
+            for term in keywords:
+                if term not in index.keys():
+                    index[term] = []
+                index[term].append(path)
+        except Exception as e:
+            errors.append((path, e))
+    return index, errors
 
 
 def draft_index_content(index):
-	terms = sorted(index.keys())
-	text = '# Keyword Index\n'
-	letter = 'A'
-	while letter <= 'Z':
-		text += f'\n## --- {letter} ---\n'
-		while terms and terms[0][0] == letter:
-			term = terms.pop(0)
-			text += f'**{term}:**\n'
-			for path in index[term]:
-				text += f'- [{path}]({path})\n'
-			else:
-				text += '\n'
-		letter = chr(ord(letter)+1)
-	return text
+    terms = sorted(index.keys())
+    text = '# Keyword Index\n'
+    letter = 'A'
+    while letter <= 'Z':
+        text += f'\n## --- {letter} ---\n'
+        while terms and terms[0][0] == letter:
+            term = terms.pop(0)
+            text += f'**{term}:**\n'
+            for path in index[term]:
+                text += f'- [{path}]({path})\n'
+            else:
+                text += '\n'
+        letter = chr(ord(letter)+1)
+    return text
 
 
 def main():
-	paths = get_snippet_filepaths(CAMPUS_DIRS)
-	index, errors = build_index(paths, PATTERN)
-	text = draft_index_content(index)
-	with open(INDEX_FILE, 'w') as index_file:
-		index_file.write(text)
-	print('\nIndexing complete!')
-	if len(errors) > 0:
-		print(f'\nThe following files could not be indexed:')
-		for error in errors:
-			print(f' - {error[0]}: {error[1]}')
-	print()
+    paths = get_snippet_filepaths(CAMPUS_DIRS)
+    index, errors = build_index(paths, PATTERN)
+    text = draft_index_content(index)
+    with open(INDEX_FILE, 'w') as index_file:
+        index_file.write(text)
+    print('\nIndexing complete!')
+    if len(errors) > 0:
+        print(f'\nThe following files could not be indexed:')
+        for error in errors:
+            print(f' - {error[0]}: {error[1]}')
+    print()
 
 
 if __name__ == "__main__":
-	main()
+    main()
--- tests.py
+++ tests.py
@@ -101,78 +101,78 @@ SAMPLE_INDEX_CONTENT = '''# Keyword Index
 
 class TestIndexer(unittest.TestCase):
 
-	def create_one_test_file(self, keywords='', path=None):
-		text = f'{SAMPLE_TEXT_PART1}{keywords}{SAMPLE_TEXT_PART2}'
-		path = TEST_SNIPPET if path is None else path
-		with open(path, 'w') as file:
-			file.write(text)
+    def create_one_test_file(self, keywords='', path=None):
+        text = f'{SAMPLE_TEXT_PART1}{keywords}{SAMPLE_TEXT_PART2}'
+        path = TEST_SNIPPET if path is None else path
+        with open(path, 'w') as file:
+            file.write(text)
 
-	def create_test_dirs(self):
-		dirs = []
-		os.mkdir(TEST_DIR)
-		for campus in indexer.CAMPUS_DIRS:
-			dir = (os.path.join(TEST_DIR, campus))
-			os.mkdir(dir)
-			dirs.append(dir)
-		return dirs
+    def create_test_dirs(self):
+        dirs = []
+        os.mkdir(TEST_DIR)
+        for campus in indexer.CAMPUS_DIRS:
+            dir = (os.path.join(TEST_DIR, campus))
+            os.mkdir(dir)
+            dirs.append(dir)
+        return dirs
 
-	def create_multiple_test_files(self, dirs):
-		paths = []
-		for dir in dirs:
-			path = os.path.join(dir, TEST_SNIPPET)
-			paths.append(path)
-			keywords = f'## Keywords\n\n{os.path.split(dir)[1]}\n\n'
-			self.create_one_test_file(keywords=keywords, path=path)
-		return paths
+    def create_multiple_test_files(self, dirs):
+        paths = []
+        for dir in dirs:
+            path = os.path.join(dir, TEST_SNIPPET)
+            paths.append(path)
+            keywords = f'## Keywords\n\n{os.path.split(dir)[1]}\n\n'
+            self.create_one_test_file(keywords=keywords, path=path)
+        return paths
 
-	def tearDown(self):
-		if os.path.exists(TEST_SNIPPET):
-			os.remove(TEST_SNIPPET)
-		if os.path.exists(TEST_DIR):
-			shutil.rmtree(TEST_DIR)
+    def tearDown(self):
+        if os.path.exists(TEST_SNIPPET):
+            os.remove(TEST_SNIPPET)
+        if os.path.exists(TEST_DIR):
+            shutil.rmtree(TEST_DIR)
 
-	def test_extract_keywords_normal(self):
-		text = '## Keywords\n\nranking, HathiTrust, ILL, "welcome screen", \'nav bar\', "advanced search" \n\n'
-		self.create_one_test_file(text)
-		result = indexer.extract_keywords(TEST_SNIPPET, indexer.PATTERN)
-		expected = ['Ranking', 'HathiTrust', 'ILL', 'Welcome screen', 'Nav bar', 'Advanced search']
-		self.assertEqual(result, expected)
+    def test_extract_keywords_normal(self):
+        text = '## Keywords\n\nranking, HathiTrust, ILL, "welcome screen", \'nav bar\', "advanced search" \n\n'
+        self.create_one_test_file(text)
+        result = indexer.extract_keywords(TEST_SNIPPET, indexer.PATTERN)
+        expected = ['Ranking', 'HathiTrust', 'ILL', 'Welcome screen', 'Nav bar', 'Advanced search']
+        self.assertEqual(result, expected)
 
-	def test_extract_keywords_empty(self):
-		self.create_one_test_file('')
-		with self.assertRaises(Exception):
-			indexer.extract_keywords(TEST_SNIPPET, indexer.PATTERN)
+    def test_extract_keywords_empty(self):
+        self.create_one_test_file('')
+        with self.assertRaises(Exception):
+            indexer.extract_keywords(TEST_SNIPPET, indexer.PATTERN)
 
-	def test_get_snippet_filepaths(self):
-		dirs = self.create_test_dirs()
-		paths = self.create_multiple_test_files(dirs)
-		result = indexer.get_snippet_filepaths(dirs)
-		self.assertEqual(result, paths)
+    def test_get_snippet_filepaths(self):
+        dirs = self.create_test_dirs()
+        paths = self.create_multiple_test_files(dirs)
+        result = indexer.get_snippet_filepaths(dirs)
+        self.assertEqual(result, paths)
 
-	def test_build_index(self):
-		dirs = self.create_test_dirs()
-		paths = self.create_multiple_test_files(dirs)
-		result, errors = indexer.build_index(paths, indexer.PATTERN)
-		expected = {
-			'Ucb': ['test/ucb/test_snippet.md'],
-			'Ucd': ['test/ucd/test_snippet.md'],
-			'Uci': ['test/uci/test_snippet.md'],
-			'Ucla': ['test/ucla/test_snippet.md'],
-			'Ucm': ['test/ucm/test_snippet.md'],
-			'Ucr': ['test/ucr/test_snippet.md'],
-			'Ucsb': ['test/ucsb/test_snippet.md'],
-			'Ucsc': ['test/ucsc/test_snippet.md'],
-			'Ucsd': ['test/ucsd/test_snippet.md'],
-			'Ucsf': ['test/ucsf/test_snippet.md']
-		}
-		self.assertEqual(result, expected)
+    def test_build_index(self):
+        dirs = self.create_test_dirs()
+        paths = self.create_multiple_test_files(dirs)
+        result, errors = indexer.build_index(paths, indexer.PATTERN)
+        expected = {
+            'Ucb': ['test/ucb/test_snippet.md'],
+            'Ucd': ['test/ucd/test_snippet.md'],
+            'Uci': ['test/uci/test_snippet.md'],
+            'Ucla': ['test/ucla/test_snippet.md'],
+            'Ucm': ['test/ucm/test_snippet.md'],
+            'Ucr': ['test/ucr/test_snippet.md'],
+            'Ucsb': ['test/ucsb/test_snippet.md'],
+            'Ucsc': ['test/ucsc/test_snippet.md'],
+            'Ucsd': ['test/ucsd/test_snippet.md'],
+            'Ucsf': ['test/ucsf/test_snippet.md']
+        }
+        self.assertEqual(result, expected)
 
-	def test_draft_index_content(self):
-		index = {
-			'Bugs': ['ucla/boolean_search.md', 'uci/boolean_search.md'],
-			'HathiTrust': ['ucla/hathitrust.md', 'ucsb/hathitrust2.md'],
-			'Something': ['ucla/something.md'],
-			'Something else': ['ucla/something_else.md']
-		}
-		result = indexer.draft_index_content(index)
-		self.assertEqual(result, SAMPLE_INDEX_CONTENT)
+    def test_draft_index_content(self):
+        index = {
+            'Bugs': ['ucla/boolean_search.md', 'uci/boolean_search.md'],
+            'HathiTrust': ['ucla/hathitrust.md', 'ucsb/hathitrust2.md'],
+            'Something': ['ucla/something.md'],
+            'Something else': ['ucla/something_else.md']
+        }
+        result = indexer.draft_index_content(index)
+        self.assertEqual(result, SAMPLE_INDEX_CONTENT)
